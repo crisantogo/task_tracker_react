6fbbc04a10e540804502ad2f571516f2d20f00aa
crisantogo <christopher.gopeesingh@gmail.com>
2022-10-14 13:35:52 -0400

    Added POST method

--- src/App.js
+++ src/App.js
@@ -23,14 +23,32 @@ function App() {
 
     return data
   }
+  //Fetch Task
+  const fetchTask = async(id) =>{
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 
 
   
   //Add Task
-  const addTask=(task)=>{
-    const id= Math.floor(Math.random()*10000)+1
-    const newTask  = {id, ...task}
-    setTasks([...tasks, newTask])
+  const addTask=async(task)=>{
+    const res=await fetch('http://localhost:5000/tasks',{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data=await res.json()
+
+    setTasks([...tasks, data])
+    // const id= Math.floor(Math.random()*10000)+1
+    // const newTask  = {id, ...task}
+    // setTasks([...tasks, newTask])
    
   }
 
